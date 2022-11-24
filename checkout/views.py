af9ebfd26605e978d85ba8397e667d6bc564e9b1
ShonaOB <shona-obriendev@outlook.com>
2022-11-24 12:23:31 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51A37E6LQ3VKxbQZy7xgllEfJFBscDKyPKONMfim9ajpL9gO9I8efQv1XweUKRT7kOivJtGQOoc8N9kYhk8vmZQ1W00efOJtTIJ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
