2757a2ab9d2af7929161082bb1eae6016cca4f9b
ShonaOB <shona-obriendev@outlook.com>
2022-11-26 12:35:27 +0000

    Aded checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         """
         Updated grand total each time a line item is added
         """
-        self.order_total = self.lineitems.aggregate(Sum('ineitem_total'))['lineitem_total_sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -62,12 +62,12 @@ class OrderLineItem(models.Model):
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
-            """
-            Override the original save method to set the order
-            number, if it hasn't been set already
-            """
-            self.lineitem_total = self.product.price * self.quantity
-            super().save(*args, **kwargs)
+        """
+        Override the original save method to set the order
+        number, if it hasn't been set already
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
