36350af170e3e5a543969891dee0f5bb31c0f109
ShonaOB <shona-obriendev@outlook.com>
2022-12-01 14:30:06 +0000

    Added wishlist context

---
+++ wishlist/contexts.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+def wishlist_contents(request):
+
+    wishlist_items = []
+    product_count = 0
+
+    context = {
+        'wishlist_items': wishlist_items,
+        'product_count': product_count,
+    }
+
+    return context
