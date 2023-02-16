eeddabfc911d6535f801cba9a5239c7c2438e50f
ShonaOB <shona-obriendev@outlook.com>
2023-02-16 08:53:23 +0000

    Added post_detail html, views and url. Added link to blog page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
     path('add_post/', views.AddPost.as_view(), name='add_post'),
+    path('<post_id>/', views.post_detail, name='post_detail'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,25 +7,35 @@ from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-
-
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.order_by('-created_on')
     template_name = 'blog.html'
 
 
-class AddPost(LoginRequiredMixin,generic.CreateView):
+class AddPost(LoginRequiredMixin, generic.CreateView):
     """
     adding a blog post
     """
     model = Post
     template_name = 'add_post.html'
-    fields = ['title','content', 'image']
-    
+    fields = ['title', 'content', 'image']
+
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('blog')
+
+
+def post_detail(request, post_id):
+    """ A view to show individual post """
+
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'post_detail.html', context)
